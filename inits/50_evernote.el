fd079d7cc46f118537f27c8a01cf4e45d44389a3
NAGATA Hiroaki <nagata@handlename.net>
2010-12-11 16:16:47 +0900

    evernote.el

---
+++ inits/50_evernote.el
@@ -0,0 +1,7 @@
+(require 'evernote-mode)
+
+(global-set-key "\C-cec" 'evernote-create-note)
+(global-set-key "\C-ceo" 'evernote-open-note)
+(global-set-key "\C-ces" 'evernote-search-notes)
+(global-set-key "\C-ceS" 'evernote-do-saved-search)
+(global-set-key "\C-cew" 'evernote-write-note)
