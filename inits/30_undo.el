79617e20ee0ad76e800239667a3db41f42db7c2b
NAGATA Hiroaki <nagata@handlename.net>
2011-01-17 22:09:26 +0900

    undo-treeとundohist

---
+++ inits/30_undo.el
@@ -0,0 +1,14 @@
+;; undo-tree
+;; INSTALL
+;; (install-elisp "http://www.dr-qubit.org/undo-tree/undo-tree.el")
+
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+
+;; undohist
+;; INSTALL
+;;(install-elisp "http://cx4a.org/pub/undohist.el")
+
+(require 'undohist)
+(undohist-initialize)
