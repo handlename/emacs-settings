bd2fdb220ddf048e6fa2d29a2b9d4ebd83ab59f5
NAGATA Hiroaki <nagata@handlename.net>
2011-01-17 21:44:55 +0900

    flymakeのエラー表示方法変更

--- inits/50_flymake.el
+++ inits/50_flymake.el
@@ -26,6 +26,30 @@
 ;;   (set (make-local-variable 'post-command-hook)
 ;;        (add-hook 'post-command-hook 'flymake-popup-err-message)))
 
+;; エラー表示
+;; INSTALL
+;; (install-elisp "http://nschum.de/src/emacs/fringe-helper/fringe-helper.el")
+(require 'fringe-helper)
+(set-face-background 'flymake-errline nil)
+(set-face-underline 'flymake-errline "#FF0000")
+(defvar flymake-fringe-overlays nil)
+(make-variable-buffer-local 'flymake-fringe-overlays)
+(defadvice flymake-make-overlay (after add-to-fringe first
+                                 (beg end tooltip-text face mouse-face)
+                                 activate compile)
+  (push (fringe-helper-insert-region
+         beg end
+         (fringe-lib-load (if (eq face 'flymake-errline)
+                              fringe-lib-exclamation-mark
+                            fringe-lib-question-mark))
+         'left-fringe 'font-lock-warning-face)
+        flymake-fringe-overlays))
+(defadvice flymake-delete-own-overlays (after remove-from-fringe activate
+                                        compile)
+  (mapc 'fringe-helper-remove flymake-fringe-overlays)
+  (setq flymake-fringe-overlays nil))
+
+
 ;; キーバインド
 (global-set-key "\M-p" 'flymake-goto-prev-error)
 (global-set-key "\M-n" 'flymake-goto-next-error)
