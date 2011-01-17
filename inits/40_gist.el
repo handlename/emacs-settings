e07fb3b8ff35864c929c89e3ed740298b20954b9
NAGATA Hiroaki <nagata@handlename.net>
2011-01-17 21:47:55 +0900

    gistとanything-gist追加

---
+++ inits/40_gist.el
@@ -0,0 +1,10 @@
+;; INSTALL
+;; (install-elisp "https://github.com/defunkt/gist.el/raw/master/gist.el")
+
+(require 'gist)
+
+;; anything-gist
+;; INSTALL
+;;(install-elisp "https://gist.github.com/raw/467982/a61f47a94f43d24fd10d89e35d49ed41e36a2038/anything-gist.el")
+
+(require 'anything-gist)
