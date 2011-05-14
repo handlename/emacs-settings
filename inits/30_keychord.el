7704255252e2b6a6ed5f3d1b6154542b200d01ec
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 05:05:27 +0900

    keychord設定

--- inits/30_keychord.el
+++ inits/30_keychord.el
@@ -6,6 +6,7 @@
 (setq key-chord-two-keys-delay 0.04)
 (key-chord-mode 1)
 
-(key-chord-define-global "_=" 'org-remember)
-(key-chord-define-global "s-" 'whitespace-cleanup)
+(key-chord-define-global "_=" 'open-junk-file)
+(key-chord-define-global "s-" 'org-capture)
+(key-chord-define-global "ao" 'whitespace-cleanup)
 (key-chord-define-global ",'" 'align)
