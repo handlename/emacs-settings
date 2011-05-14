c52424ed556e45adf0d4428bd00f8a7260c51b38
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 04:56:04 +0900

    magitを有効に

--- inits/30_magit.el
+++ inits/30_magit.el
@@ -1,9 +1,9 @@
 ;; INSTALL
-;; (install-elisp "http://www.gentei.org/~yuuji/software/revive.el")
+;; $ git clone git://github.com/jdhuntington/magit.git
+;; $ ./autogen.sh
+;; $ ./configure --prefix=$HOME/.emacs.d/site-lisp/magit
+;; $ make
+;; $ sudo make install
 
-(autoload 'save-current-configuration "revive" "Save status" t)
-(autoload 'resume "revive" "Resume Emacs" t)
-(autoload 'wipe "revive" "Wipe emacs" t)
-(define-key global-map (kbd "C-x S") 'save-current-configuration)
-(define-key global-map (kbd "C-x F") 'resume)
-(add-hook 'kill-emacs-hook 'save-current-configuration)
+;(add-to-list 'load-path "~/.emacs.d/site-lisp/magit/share/emacs/site-lisp")
+(require 'magit)
