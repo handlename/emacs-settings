679d9f9c8dfb1392a75e9210548f80aee2e09016
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 05:07:51 +0900

    携帯コーディング用関数

--- inits/20_custom_commands.el
+++ inits/20_custom_commands.el
@@ -6,3 +6,24 @@
   (interactive)
   (loop for buffer being the buffers
         do (kill-buffer buffer)))
+
+;; ケータイコーディング用
+(defun ktai-hankaku-katakana-region (start end)
+  (interactive "r")
+  (while (string-match
+          "[０-９Ａ-Ｚａ-ｚァ-ンー]+"
+          (buffer-substring start end))
+    (save-excursion
+      (japanese-hankaku-region
+       (+ start (match-beginning 0))
+       (+ start (match-end 0))
+       ))))
+
+(defun ktai-hankaku-katakana-buffer ()
+  (interactive)
+  (ktai-hankaku-katakana-region (point-min) (point-max)))
+
+;; リージョン内の文字幅をしらべる
+(defun string-width-in-region (start end)
+  (interactive "r")
+  (princ (string-width (buffer-substring start end))))
