7ad281af507ca3a4d2e92a2f1059683506880dd2
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 05:21:13 +0900

    rst-mode追加

---
+++ inits/50_rst.el
@@ -0,0 +1,9 @@
+;; INSTALL
+;; (install-elisp "http://docutils.sourceforge.net/tools/editors/emacs/rst.el")
+
+(require 'rst)
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+		("\\.rest$" . rst-mode)) auto-mode-alist))
+(setq frame-background-mode 'dark)
+(add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
