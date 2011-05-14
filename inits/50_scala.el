45cc86778513453ed49b6de862346beb5a2d6865
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 05:01:45 +0900

    scala設定

--- inits/50_scala.el
+++ inits/50_scala.el
@@ -1,3 +1,3 @@
-(add-to-list 'load-path "~/.emacs.d/site-lisp/scala-mode")
+;(add-to-list 'load-path "~/.emacs.d/site-lisp/scala-mode")
 (require 'scala-mode-auto)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
