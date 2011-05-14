cd2abe771acb5f0b160b9a5bd446e7c487ba3a9e
NAGATA Hiroaki <nagata@handlena.me>
2011-05-15 05:19:24 +0900

    e?lisp-modeキーバインド変更

--- inits/50_elisp.el
+++ inits/50_elisp.el
@@ -1,7 +1,6 @@
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
-             (local-set-key (kbd "C-c C-e") 'eval-last-sexp)
-             (local-set-key (kbd "C-c e")   'eval-region)
+             (local-set-key (kbd "C-c e") 'eval-region)
              (setq indent-tabs-mode nil)))
 
 ;; lispxmp.el
--- inits/50_lisp.el
+++ inits/50_lisp.el
@@ -1,5 +1,4 @@
 (add-hook 'lisp-mode-hook
           '(lambda ()
-             (local-set-key (kbd "C-c C-e") 'eval-last-sexp)
-             (local-set-key (kbd "C-c e")   'eval-region)
+             (local-set-key (kbd "C-c e") 'eval-region)
              (setq indent-tabs-mode nil)))
