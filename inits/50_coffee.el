ebe03e9516c18967e6f3c83428f29c3d22331002
NAGATA Hiroaki <nagata@handlena.me>
2011-06-01 11:02:47 +0900

    CoffeeScriptの設定追加

--- inits/50_coffee.el
+++ inits/50_coffee.el
@@ -1,3 +1,15 @@
 ;; INSTALL
 ;; (install-elisp "https://github.com/defunkt/coffee-mode/raw/master/coffee-mode.el")
 
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(add-hook 'coffee-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "C-c C-c") 'coffee-compile-file)))
+
+(add-hook 'after-save-hook
+          (lambda ()
+            (if (eq major-mode 'coffee-mode)
+                (coffee-compile-file))))
