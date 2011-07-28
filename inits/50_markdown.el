3efb9767c4c2ba177a8d6cf64ef564fb961fd409
NAGATA Hiroaki <nagata@handlena.me>
2011-07-28 17:29:56 +0900

    add install command for markdown-mode

--- inits/50_markdown.el
+++ inits/50_markdown.el
@@ -1,4 +1,5 @@
 ;; markdown-mode
+;; (install-elisp "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
 
 (autoload 'markdown-mode "markdown-mode.el"
   "Major mode for editing Markdown files" t)
