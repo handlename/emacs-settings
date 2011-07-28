717fb7e2900902ff8d5e6e2c095e9f3c003bd044
NAGATA Hiroaki <nagata@handlena.me>
2011-07-28 17:26:40 +0900

    delete key bindings for elscreen

--- inits/30_elscreen.el
+++ inits/30_elscreen.el
@@ -5,5 +5,3 @@
 (load "elscreen" "ElScreen" t)
 (setq elscreen-display-tab nil) ; タブを非表示
 (define-key global-map (kbd "M-t") 'elscreen-next)
-(define-key global-map (kbd "M-C-t") 'elscreen-create)
-(define-key global-map (kbd "M-C-w") 'elscreen-kill)
