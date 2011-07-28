d23f91eb29a9bfd19487991298523629b2b63cab
NAGATA Hiroaki <nagata@handlena.me>
2011-07-28 17:36:48 +0900

    update setting for auto-complete

--- inits/60_auto-complete.el
+++ inits/60_auto-complete.el
@@ -10,38 +10,53 @@
 (global-set-key (kbd "M-_") 'auto-complete)
 (setq ac-auto-start 1)       ; 補完を開始する文字数
 (setq ac-auto-show-menu 0.2) ; 補完リストが表示されるまでの時間
-(setq ac-sources '(ac-source-yasnippet
-                   ac-source-dictionary
-                   ac-source-gtags
-                   ac-source-words-in-buffer))
 
+(defun auto-complete-init-sources ()
+  (setq ac-sources '(ac-source-yasnippet
+                     ac-source-dictionary
+                     ac-source-gtags
+                     ac-source-words-in-buffer)))
+(auto-complete-init-sources)
+  
 (add-to-list 'ac-modes 'emacs-lisp-mode)
 (add-to-list 'ac-modes 'html-helper-mode)
 (add-to-list 'ac-modes 'javascript-mode)
 (add-to-list 'ac-modes 'tmt-mode)
 (add-to-list 'ac-modes 'yaml-mode)
 
-;; ; for cperl-mode
-;; (add-hook 'cperl-mode-hook
-;;           '(lambda ()
-;;              (progn
-;;                (require 'perl-completion)
-;;                (add-to-list 'ac-sources 'ac-source-perl-completion)
-;;                (perl-completion-mode t)
-;;               )))
+;; company
+;; (install-elisp "http://nschum.de/src/emacs/company-mode/company-0.5.tar.bz2")
+
+;; ac-company
+;; (install-elisp "https://raw.github.com/buzztaiki/auto-complete/master/ac-company.el")
+(require 'ac-company)
+
+; for cperl-mode
+(setq plcmp-use-keymap nil)
+(require 'perl-completion)
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (progn
+               (auto-complete-init-sources)
+               (add-to-list 'ac-sources 'ac-source-perl-completion)
+               (perl-completion-mode t)
+              )))
 
 ; for emacs-lisp-mode
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
+             (auto-complete-init-sources)
              (add-to-list 'ac-sources 'ac-source-functions)
              (add-to-list 'ac-sources 'ac-source-symbols)))
 
 ; for yaml-mode
 (add-hook 'yaml-mode-hook
           '(lambda ()
+             (auto-complete-init-sources)
              (setq ac-sources '(ac-source-words-in-buffer))))
 
 ; for objc-mode
+(setq ac-modes (append ac-modes '(objc-mode)))
 (require 'etags-table)
 (add-to-list  'etags-table-alist
               '("\\.[mh]$" "~/.emacs.d/tags/objc.TAGS"))
@@ -54,4 +69,6 @@
   "etags をソースにする")
 (add-hook 'objc-mode-hook
           (lambda ()
-            (push 'ac-source-etags-table ac-sources)))
+           (push 'ac-source-company-xcode ac-sources)
+           (push 'ac-source-c++-keywords ac-sources)
+           (push 'ac-source-etags-table ac-sources)))
