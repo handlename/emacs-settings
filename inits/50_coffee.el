ea846165050724b1c21c599b6e4977486462a5be
NAGATA Hiroaki <nagata@handlena.me>
2011-07-28 17:29:31 +0900

    update setting for coffee-mode

--- inits/50_coffee.el
+++ inits/50_coffee.el
@@ -4,12 +4,8 @@
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+(setq coffee-cleanup-whitespace nil)
 
 (add-hook 'coffee-mode-hook
           '(lambda ()
              (local-set-key (kbd "C-c C-c") 'coffee-compile-file)))
-
-(add-hook 'after-save-hook
-          (lambda ()
-            (if (eq major-mode 'coffee-mode)
-                (coffee-compile-file))))
